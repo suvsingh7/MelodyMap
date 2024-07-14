72b908b767b222c5cc63fd256de625ab812e060c
suvsingh7 <suvendras777@gmail.com>
2024-07-14 19:29:15 +0530

    update-1

--- melodymap /src/components/SongItem.jsx
+++ melodymap /src/components/SongItem.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-//import { useNavigate } from 'react-router-dom'
 
-const AlbumItem = ({image,name,desc,id}) => {
-
-    //const navigate = useNavigate()
+const SongItem = ({name,image,desc,id}) => {
   return (
     <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
-        <img className ='rounded' src={image} alt='' />
+        <img className='rounded' src={image} alt='' />
         <p className='font-bold mt-2 mb-1'>{name}</p>
         <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
   )
 }
 
-export default AlbumItem
+export default SongItem
