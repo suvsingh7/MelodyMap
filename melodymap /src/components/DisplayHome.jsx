48987d455cb9051e8451206ba5f387f95a8fdb6a
suvsingh7 <suvendras777@gmail.com>
2024-07-14 18:50:50 +0530

    assets and display

---
+++ melodymap /src/components/DisplayHome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import AlbumItem from './Albumitem'
+
+const DisplayHome = () => {
+  return (
+    <>
+        <Navbar/>
+        <div className='my-5 font-bold text-2xl'>
+            {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+        </div>
+    </>
+  )
+}
+
+export default DisplayHome
