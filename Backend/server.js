0a3037e21433e840e813c7001836317c694381bd
suvsingh7 <suvendras777@gmail.com>
2024-07-16 23:55:35 +0530

    Backend Completed

--- Backend/server.js
+++ Backend/server.js
@@ -4,27 +4,27 @@ import connectCloudinary from "./src/config/cloudinary.js";
 import 'dotenv/config'
 import connectDB from "./src/config/mongodb.js";
 import songRouter from "./src/routes/songRoute.js";
-//import albumRouter from "./src/routes/albumRoute.js";
+import albumRouter from "./src/routes/albumRoute.js";
 
 
 
 
 
 // app config
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 connectCloudinary();
 connectDB();
 
 
 
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // Initializing Routers
 app.use("/api/song", songRouter )
-//app.use("/api/album", albumRouter )
+app.use("/api/album", albumRouter )
 
 app.get("/", (req, res) => res.send("API Working"))
 
