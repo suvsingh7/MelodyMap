5243f75cd2fcc87607b83f23883a6b80eb72a8f6
suvsingh7 <suvendras777@gmail.com>
2024-07-17 00:44:27 +0530

    Admin panel first commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-900 min-h-[100vh]'>
+
+    </div>
+  )
+}
+
+export default App
---
+++ admin/src/assets/assets.js
@@ -0,0 +1,28 @@
+import Melody_logo from './Melody_logo.png'  // app logo
+import Melody_logo_2 from './Melody_logo_2.png'
+import Melody_logo_3 from './Melody_logo_3.png'
+import Melody_logo_4 from './Melody_logo_4.png'
+
+
+import upload_area from './upload_area.png'
+import upload_added from './upload_added.png'
+import add_album from './add_album.png'
+import add_song from './add_song.png'
+import song_icon from './song_icon.png'
+import album_icon from './album_icon.png'
+import upload_song from './upload_song.png'
+
+export const assets ={
+    upload_area,
+    upload_added,
+    add_album,
+    add_song,
+    song_icon,
+    album_icon,
+    upload_song,
+    Melody_logo,
+    Melody_logo_2,
+    Melody_logo_3,
+    Melody_logo_4
+}
+
