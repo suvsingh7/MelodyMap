172b2818d40e8905d19b2837f078f6370b2fd017
suvsingh7 <suvendras777@gmail.com>
2024-07-21 12:12:28 +0530

    admin panel completed

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar w-full border-b-2 border-gray-800 px-5 sm:px-12 py-4 text-lg'>
+      <p>Admin Panel</p>
+    </div>
   )
 }
 
